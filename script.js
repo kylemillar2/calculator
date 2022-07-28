93b7ee18b2b046cdc48a3882a03c59f22dd80867
Kyle Millar <kyledmillar@gmail.com>
2022-07-28 23:27:28 +0100

    Add button functionality

--- script.js
+++ script.js
@@ -26,3 +26,48 @@ function operate(op, a, b) {
             return divide(a, b);
     }
 }
+
+function populateDisplay(event) {
+    let displayText = displaySpan.textContent;
+    let char = event.target.textContent;
+    let lastChar = displayText.slice(-1);
+    if (!(["+", "-", "*", "/"].includes(char) && ["+", "-", "*", "/"].includes(lastChar))) {
+        displayText += char;
+        displaySpan.textContent = displayText;
+    }
+}
+
+function calculate() {
+
+}
+
+function clear() {
+    displaySpan.textContent = "";
+}
+
+function del() {
+    let displayText = displaySpan.textContent;
+    displaySpan.textContent = displayText.slice(0, -1);
+}
+
+let displaySpan = document.querySelector(".display span");
+
+let buttons = document.querySelector(".buttons");
+let allButtons = buttons.querySelectorAll("button");
+allButtons.forEach(button => {
+    if (button.id === "equals") {
+        button.addEventListener("click", calculate);
+    } else {
+        button.addEventListener("click", populateDisplay);
+    }
+})
+
+let clearDel = document.querySelector(".clear-del");
+let clearDelButtons = clearDel.querySelectorAll("button");
+clearDelButtons.forEach(button => {
+    if (button.id === "clear") {
+        button.addEventListener("click", clear)
+    } else if (button.id === "del") {
+        button.addEventListener("click", del)
+    }
+})
